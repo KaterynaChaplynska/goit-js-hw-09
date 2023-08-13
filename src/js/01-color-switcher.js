2b24112f948cb07b94a65046262cae18271440f7
KaterynaChaplynska <101507992+KaterynaChaplynska@users.noreply.github.com>
2023-08-13 08:56:18 +0300

    task-01

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,27 @@
+const refs = {
+    startBtn: document.querySelector('[data-start]'),
+    stopBtn: document.querySelector('[data-stop]'),
+};
+
+let intervalId = null;
+
+refs.startBtn.addEventListener('click', () => {
+    intervalId = setInterval(() => {
+        document.body.style.backgroundColor = getRandomHexColor();
+    }, 1000);
+
+    refs.startBtn.disabled = true;
+    refs.stopBtn.disabled = false;
+});
+
+refs.stopBtn.addEventListener('click', () => {
+    clearInterval(intervalId);
+
+    refs.startBtn.disabled = false;
+    refs.stopBtn.disabled = true;
+});
+
+function getRandomHexColor() {
+    return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
 
